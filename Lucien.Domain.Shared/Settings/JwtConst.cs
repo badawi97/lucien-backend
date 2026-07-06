e18d18ee3cb1db0f49cc4b4549016602dd787027
Khalid Badawi <kbadawi@isb.sa>
2026-07-06 19:58:09 +0300

    fix Audience onrender

--- Lucien.Domain.Shared/Settings/JwtConst.cs
+++ Lucien.Domain.Shared/Settings/JwtConst.cs
@@ -5,6 +5,7 @@
         public const string Issuer = "Issuer";
         public const string SecretKey = "SecretKey";
         public const string Audience = "Audience";
+        //public const string Audience = "AudienceDEV";
         public const string JwtSettings = "JwtSettings";
         public const string RefreshExpiryDays = "RefreshExpiryDays";
         public const string ExpiryMinutes = "ExpiryMinutes";
